67316b24c6e02be6a2fd491517e4e44148d763d0
ttinies <ttinies.v@gmail.com>
2018-06-10 11:46:10 -0600

    - now() now accepts a specific timestamp, not just current time

--- dateFormat.py
+++ dateFormat.py
@@ -6,8 +6,10 @@ from __future__ import print_function # python 2/3 compatibility
 import datetime
 
 ################################################################################
-def now():
-    now = datetime.datetime.utcnow().replace(microsecond=0)
+def now(givenTime=None):
+    if givenTime:   usableTime = datetime.datetime.utcfromtimestamp(givenTime)
+    else:           usableTime = datetime.datetime.utcnow()
+    now = usableTime.replace(microsecond=0)
     now = now.isoformat("_").replace(":", "_")
     return now
 
