f13c7dd03c96f927227352658b011a6fc2c29758
ttinies <ttinies.v@gmail.com>
2018-06-22 22:31:05 -0600

    - notes about potential future UI development

---
+++ uiControl.py
@@ -0,0 +1,36 @@
+"""
+platform.system()
+    Linux: Linux
+    Mac: Darwin
+    Windows: Windows
+
+can change the windows server process name (PyPi modules)
+    wmi-client-wrapper (0.0.12)      - Linux-only wrapper around wmi-client for WMI (Windows)
+    WMI (1.4.9)                      - Windows Management Instrumentation
+    wmi-client-wrapper-py3 (2018.3)  - A fork of Bryan Bishop's wmi-client-wrapper extended to include Python3 support
+
+https://opensource.com/resources/python/gui-frameworks
+
+
+> wrap/utilize game lobby / config
+
+
+        ** causes unofficial, custom game
+            desired map name
+            desired opponent playerID
+            fog of war
+            realtime or step rate
+
+
+
+    UI to select options
+        select game critera
+            desiredRace
+            agent to play
+            generated comm
+
+"""
+
+
+import platform
+
