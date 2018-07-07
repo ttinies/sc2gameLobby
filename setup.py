13df7aed17f04f76aa2fd2758845a35f1bc3aebd
Mike Henry <mhenry@handycodejob.com>
2018-07-07 16:58:36 -0600

    some pep8 changes

--- setup.py
+++ setup.py
@@ -12,25 +12,23 @@ from shutil import rmtree
 from setuptools import find_packages, setup, Command
 
 # Package meta-data.
-NAME = 'sc2gameLobby'
-DESCRIPTION = 'Game lobby for SC2 AI and AI assisted games'
-URL = 'https://github.com/ttinies/sc2gameLobby'
-EMAIL = 'chageme'
-AUTHOR = 'change me'
-REQUIRES_PYTHON = '>2.6'
+NAME = "sc2gameLobby"
+DESCRIPTION = "Game lobby for SC2 AI and AI assisted games"
+URL = "https://github.com/ttinies/sc2gameLobby"
+EMAIL = "chageme"
+AUTHOR = "change me"
+REQUIRES_PYTHON = ">2.6"
 VERSION = None
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    'six',
-    'pySC2',
-    's2clientprotocol',
-    'portpicker'
-    #sc2common,
-    #sc2ladderMgmt,
-    #sc2players
-
-
+    "six",
+    "pySC2",
+    "s2clientprotocol",
+    "portpicker"
+    # sc2common,
+    # sc2ladderMgmt,
+    # sc2players
 ]
 
 # The rest you shouldn't have to touch too much :)
@@ -43,28 +41,28 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 # Import the README and use it as the long-description.
 # Note: this will only work if 'README.md' is present in your MANIFEST.in file!
-with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = '\n' + f.read()
+with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = "\n" + f.read()
 
 # Load the package's __version__.py module as a dictionary.
 about = {}
 if not VERSION:
-    with open(os.path.join(here, NAME, '__version__.py')) as f:
+    with open(os.path.join(here, NAME, "__version__.py")) as f:
         exec(f.read(), about)
 else:
-    about['__version__'] = VERSION
+    about["__version__"] = VERSION
 
 
 class UploadCommand(Command):
     """Support setup.py upload."""
 
-    description = 'Build and publish the package.'
+    description = "Build and publish the package."
     user_options = []
 
     @staticmethod
     def status(s):
         """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
+        print("\033[1m{0}\033[0m".format(s))
 
     def initialize_options(self):
         pass
@@ -74,20 +72,20 @@ class UploadCommand(Command):
 
     def run(self):
         try:
-            self.status('Removing previous builds…')
-            rmtree(os.path.join(here, 'dist'))
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
         except OSError:
             pass
 
-        self.status('Building Source and Wheel (universal) distribution…')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
 
-        self.status('Uploading the package to PyPi via Twine…')
-        os.system('twine upload dist/*')
+        self.status("Uploading the package to PyPi via Twine…")
+        os.system("twine upload dist/*")
 
-        self.status('Pushing git tags…')
-        os.system('git tag v{0}'.format(about['__version__']))
-        os.system('git push --tags')
+        self.status("Pushing git tags…")
+        os.system("git tag v{0}".format(about["__version__"]))
+        os.system("git push --tags")
 
         sys.exit()
 
@@ -95,37 +93,32 @@ class UploadCommand(Command):
 # Where the magic happens:
 setup(
     name=NAME,
-    version=about['__version__'],
+    version=about["__version__"],
     description=DESCRIPTION,
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     author=AUTHOR,
     author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
     url=URL,
-    packages=find_packages(exclude=('tests',)),
-    entry_points={
-         'console_scripts': ['sc2gameLobby=sc2gameLobby.cli:main'],
-    },
+    packages=find_packages(exclude=("tests",)),
+    entry_points={"console_scripts": ["sc2gameLobby=sc2gameLobby.cli:main"]},
     install_requires=REQUIRED,
     include_package_data=True,
-    package_data={'sc2gameLobby': 'jsonData/versions.json'},
-    license='Apache License 2.0',
+    package_data={"sc2gameLobby": "jsonData/versions.json"},
+    license="Apache License 2.0",
     classifiers=[
         # Trove classifiers
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Games/Entertainment :: Real Time Strategy',
-        'Topic :: Scientific/Engineering :: Artificial Intelligence'
-
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Games/Entertainment :: Real Time Strategy",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
     # $ setup.py publish support.
-    cmdclass={
-        'upload': UploadCommand,
-    },
+    cmdclass={"upload": UploadCommand},
 )
