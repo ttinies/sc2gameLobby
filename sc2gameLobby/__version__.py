3343d177ba868df65872ffca269c85a63cdaaca5
ttinies <ttinies.v@gmail.com>
2018-08-12 15:24:19 -0600

    - updated to 0.7.4

--- sc2gameLobby/__version__.py
+++ sc2gameLobby/__version__.py
@@ -17,6 +17,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-VERSION = (0, 7, 4)
+VERSION = (0, 7, 5)
 
 __version__ = '.'.join(map(str, VERSION))
