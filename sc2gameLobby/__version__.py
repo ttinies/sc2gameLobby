1f6b0318c50e523d9c155c1db3ee64812de28181
ttinies <ttinies.v@gmail.com>
2018-10-07 10:13:42 -0600

    - version bump

--- sc2gameLobby/__version__.py
+++ sc2gameLobby/__version__.py
@@ -17,6 +17,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-VERSION = (1, 1, 2)
+VERSION = (1, 1, 3)
 
 __version__ = '.'.join(map(str, VERSION))
