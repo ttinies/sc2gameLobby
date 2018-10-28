1a49920f8c750c089e38cbd23e856257981333e8
ttinies <ttinies.v@gmail.com>
2018-10-28 00:07:04 -0600

    - added version support for 4.6.2.69232

--- sc2gameLobby/__version__.py
+++ sc2gameLobby/__version__.py
@@ -17,6 +17,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-VERSION = (1, 1, 9)
+VERSION = (1, 1, 10)
 
 __version__ = '.'.join(map(str, VERSION))
